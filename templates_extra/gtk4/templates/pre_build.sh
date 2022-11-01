556b8bdfabeca5ef47c3c7d000a523be09c98cbe
actionless <actionless.loveless@gmail.com>
2022-11-01 01:57:01 +0100

    fix(templates_extra: gtk4): all other buttons' hover background - 2

--- templates_extra/gtk4/templates/pre_build.sh
+++ templates_extra/gtk4/templates/pre_build.sh
@@ -18,5 +18,7 @@ sassc -a -M -t compact ./base.scss "$BASE_CSS_PATH"
 cd "$SCRIPT_DIR"
 cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$BUTTONS_TEMPLATE_PATH" \
 | sed \
-	-e '/.* background\(-color\|\): alpha(currentColor,[0-9.]\+); .*/d' \
+	-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
+	-e '/popover.*{ background\(-color\|\): alpha(currentColor,[0-9.]\+); }/d' \
+	-e '/.*{ background-color: alpha(currentColor,[0-9.]\+); box-shadow: none; }/d' \
 > "$RESULT_TEMPLATE_PATH"
