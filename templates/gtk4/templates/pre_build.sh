ed620b36d7e4aee2fac6f4c8262aeef43020ff8d
actionless <actionless.loveless@gmail.com>
2022-11-01 01:29:59 +0100

    fix(templates_extra: gtk4): header buttons' hover background

--- templates/gtk4/templates/pre_build.sh
+++ templates/gtk4/templates/pre_build.sh
@@ -16,4 +16,7 @@ cd "${LIBADWAITA_DIR}/src/stylesheet"
 sassc -a -M -t compact ./base.scss "$BASE_CSS_PATH"
 
 cd "$SCRIPT_DIR"
-cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$BUTTONS_TEMPLATE_PATH" > "$RESULT_TEMPLATE_PATH"
+cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$BUTTONS_TEMPLATE_PATH" \
+| sed \
+	-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
+> "$RESULT_TEMPLATE_PATH"
