8ab02953fb9d82a03f8bc28d25383be1bb4eedd7
actionless <actionless.loveless@gmail.com>
2024-06-19 01:29:26 +0200

    feat(templates_extra): add gtk3-oodwaita (based on adw-gtk3) (fixes: https://github.com/themix-project/themix-gui/issues/167)

---
+++ templates_extra/gtk3-oodwaita/templates/pre_build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+set -x
+
+SCRIPT_DIR="$(readlink -e "$(dirname "${0}")")"
+
+GIT_CLONE_ROOT="$HOME/tmp"
+mkdir -p "$GIT_CLONE_ROOT"
+cd "$GIT_CLONE_ROOT"
+if [[ ! -d "$GIT_CLONE_ROOT/adw-gtk3" ]] ; then
+	git clone https://github.com/lassekongo83/adw-gtk3
+else
+	cd adw-gtk3
+	git pull origin main
+fi
+
+ADW_GTK3_DIR="$(readlink -e ~/tmp/adw-gtk3)"
+BASE_CSS_PATH="${SCRIPT_DIR}/gtk.css"
+
+BASE_TEMPLATE_PATH="${SCRIPT_DIR}/themix.mustache.in"
+BUTTONS_TEMPLATE_PATH="${SCRIPT_DIR}/themix_buttons.mustache.in"
+
+RESULT_TEMPLATE_PATH="${SCRIPT_DIR}/gtk.mustache"
+
+
+cd "${ADW_GTK3_DIR}/gtk/src/adw-gtk3/gtk-3.0"
+sassc -a -M -t compact ./gtk.scss "$BASE_CSS_PATH"
+
+cd "$SCRIPT_DIR"
+cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$BUTTONS_TEMPLATE_PATH" \
+| sed \
+	-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
+	-e '/popover.*{ background\(-color\|\): alpha(currentColor,[0-9.]\+); }/d' \
+	-e '/.*{ background-color: alpha(currentColor,[0-9.]\+); box-shadow: none; }/d' \
+	-e '/.*assets\/.*}.*/d' \
+> "$RESULT_TEMPLATE_PATH"
