f6737dd5c1d2b7960dc1c1b221d5bc37a96cecc8
actionless <actionless.loveless@gmail.com>
2024-07-15 22:05:48 +0200

    chore: sync mirror(gtk{3,4}-oodwaita, helix, qt5ct)

--- templates/gtk4-oodwaita/templates/pre_build.sh
+++ templates/gtk4-oodwaita/templates/pre_build.sh
@@ -21,7 +21,8 @@ LIBADWAITA_DIR="$(readlink -e ~/tmp/libadwaita)"
 BASE_CSS_PATH="${SCRIPT_DIR}/libadwaita.css"
 
 BASE_TEMPLATE_PATH="${SCRIPT_DIR}/themix.mustache.in"
-BUTTONS_TEMPLATE_PATH="${SCRIPT_DIR}/themix_buttons.mustache.in"
+TEMPLATE_PATH_BUTTONS="${SCRIPT_DIR}/themix_buttons.mustache.in"
+TEMPLATE_PATH_CHECKRADIO="${SCRIPT_DIR}/themix_checkradio.mustache.in"
 
 RESULT_TEMPLATE_PATH="${SCRIPT_DIR}/gtk.mustache"
 
@@ -30,9 +31,10 @@ cd "${LIBADWAITA_DIR}/src/stylesheet"
 sassc -a -M -t compact ./base.scss "$BASE_CSS_PATH"
 
 cd "$SCRIPT_DIR"
-cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$BUTTONS_TEMPLATE_PATH" \
+cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$TEMPLATE_PATH_BUTTONS" "$TEMPLATE_PATH_CHECKRADIO" \
 | sed \
 	-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
 	-e '/popover.*{ background\(-color\|\): alpha(currentColor,[0-9.]\+); }/d' \
 	-e '/.*{ background-color: alpha(currentColor,[0-9.]\+); box-shadow: none; }/d' \
+	-e '/.*assets\/.*}.*/d' \
 > "$RESULT_TEMPLATE_PATH"
