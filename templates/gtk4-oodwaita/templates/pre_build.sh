b93d9f19db1a70a669db8e9f1015a762da209c26
actionless <actionless.loveless@gmail.com>
2024-09-22 00:10:38 +0200

    chore: sync mirror (conemu, gtk4-oodwaita, termite, tmux, xresources)

--- templates/gtk4-oodwaita/templates/pre_build.sh
+++ templates/gtk4-oodwaita/templates/pre_build.sh
@@ -2,39 +2,54 @@
 set -ueo pipefail
 set -x
 
+
+VERSIONS=(1.6.0 1.5.2)
+
 SCRIPT_DIR="$(readlink -e "$(dirname "${0}")")"
+CONFIG_PATH="${SCRIPT_DIR}/config.yaml"
 
-GIT_CLONE_ROOT="$HOME/tmp"
-mkdir -p "$GIT_CLONE_ROOT"
-cd "$GIT_CLONE_ROOT"
-if [[ ! -d "$GIT_CLONE_ROOT/libadwaita" ]] ; then
-	git clone https://gitlab.gnome.org/GNOME/libadwaita
-else
-	cd libadwaita
-	git checkout main
-	git pull origin main
-	git fetch --tags
-	git checkout 1.5.2
-fi
+echo -ne > "$CONFIG_PATH"
 
-LIBADWAITA_DIR="$(readlink -e ~/tmp/libadwaita)"
-BASE_CSS_PATH="${SCRIPT_DIR}/libadwaita.css"
+for version in "${VERSIONS[@]}" ; do
 
-BASE_TEMPLATE_PATH="${SCRIPT_DIR}/themix.mustache.in"
-TEMPLATE_PATH_BUTTONS="${SCRIPT_DIR}/themix_buttons.mustache.in"
-TEMPLATE_PATH_CHECKRADIO="${SCRIPT_DIR}/themix_checkradio.mustache.in"
+	GIT_CLONE_ROOT="$HOME/tmp"
+	LIBADWAITA_DIR="$GIT_CLONE_ROOT/libadwaita-${version}"
+	mkdir -p "$GIT_CLONE_ROOT"
+	cd "$GIT_CLONE_ROOT"
+	if [[ ! -d "$LIBADWAITA_DIR" ]] ; then
+		git clone https://gitlab.gnome.org/GNOME/libadwaita.git "libadwaita-${version}"
+	else
+		cd "$LIBADWAITA_DIR"
+		git checkout main
+		git pull origin main
+		git fetch --tags
+		git checkout "$version"
+	fi
 
-RESULT_TEMPLATE_PATH="${SCRIPT_DIR}/gtk.mustache"
+	BASE_CSS_PATH="${SCRIPT_DIR}/libadwaita${version}.css"
+
+	BASE_TEMPLATE_PATH="${SCRIPT_DIR}/themix.mustache.in"
+	TEMPLATE_PATH_BUTTONS="${SCRIPT_DIR}/themix_buttons.mustache.in"
+	TEMPLATE_PATH_CHECKRADIO="${SCRIPT_DIR}/themix_checkradio.mustache.in"
+
+	RESULT_TEMPLATE_PATH="${SCRIPT_DIR}/gtk4-libadwaita${version}.mustache"
 
 
-cd "${LIBADWAITA_DIR}/src/stylesheet"
-sassc -a -M -t compact ./base.scss "$BASE_CSS_PATH"
+	cd "${LIBADWAITA_DIR}/src/stylesheet"
+	sassc -a -M -t compact ./base.scss "$BASE_CSS_PATH"
 
-cd "$SCRIPT_DIR"
-cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$TEMPLATE_PATH_BUTTONS" "$TEMPLATE_PATH_CHECKRADIO" \
-| sed \
-	-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
-	-e '/popover.*{ background\(-color\|\): alpha(currentColor,[0-9.]\+); }/d' \
-	-e '/.*{ background-color: alpha(currentColor,[0-9.]\+); box-shadow: none; }/d' \
-	-e '/.*assets\/.*}.*/d' \
-> "$RESULT_TEMPLATE_PATH"
+	cd "$SCRIPT_DIR"
+	cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$TEMPLATE_PATH_BUTTONS" "$TEMPLATE_PATH_CHECKRADIO" \
+	| sed \
+		-e '/.*infobar.*:hover.*:hover.*:hover.*{.*alpha(currentColor.*}.*/d' \
+		-e '/popover.*{ background\(-color\|\): alpha(currentColor,[0-9.]\+); }/d' \
+		-e '/.*{ background-color: alpha(currentColor,[0-9.]\+); box-shadow: none; }/d' \
+		-e '/.*assets\/.*}.*/d' \
+	> "$RESULT_TEMPLATE_PATH"
+
+	echo "gtk4-libadwaita${version}:
+  extension: /gtk-4.0/gtk.css
+  output: .themes/
+  force_filename:" >> "$CONFIG_PATH"
+
+done
