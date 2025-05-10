62013f166000ba0e6510053acc152b18d1351ee2
actionless <actionless.loveless@gmail.com>
2025-05-10 15:33:28 +0200

    chore(templates_extra: gtk3-oodwaita): update to the latest version

--- templates_extra/gtk3-oodwaita/templates/pre_build.sh
+++ templates_extra/gtk3-oodwaita/templates/pre_build.sh
@@ -24,8 +24,8 @@ TEMPLATE_PATH_CHECKRADIO="${SCRIPT_DIR}/themix_checkradio.mustache.in"
 RESULT_TEMPLATE_PATH="${SCRIPT_DIR}/gtk.mustache"
 
 
-cd "${ADW_GTK3_DIR}/gtk/src/adw-gtk3/gtk-3.0"
-sassc -a -M -t compact ./gtk.scss "$BASE_CSS_PATH"
+cd "${ADW_GTK3_DIR}/src/sass"
+sass --no-source-map --style=expanded ./gtk.scss "$BASE_CSS_PATH"
 
 cd "$SCRIPT_DIR"
 cat "$BASE_CSS_PATH" "$BASE_TEMPLATE_PATH" "$TEMPLATE_PATH_BUTTONS" "$TEMPLATE_PATH_CHECKRADIO" \
